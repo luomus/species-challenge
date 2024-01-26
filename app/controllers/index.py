beff2e003e6187ca9038f26f3713c06e5ce84de9
Mikko Heikkinen <hawk@biomi.org>
2024-01-27 01:22:58 +0200

    Add function to make challenge list

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -7,8 +7,15 @@ def get_challenges(year):
     with common_db.connection() as conn:
         query = "SELECT * FROM challenges WHERE year = %s"
         data = common_db.select(conn, query, params)
-    print(data)
     return data
+
+
+def create_challenges_html(challenges):
+    html = "<ul>\n"
+    for challenge in challenges:
+        html += f"<li><a href='{ challenge['id'] }'>{ challenge['name'] }</a></li>\n"
+    html += "</ul>\n"
+    return html
 
 
 def main():
@@ -16,6 +23,7 @@ def main():
     html["hello"] = "Hoi "
 
     challenges = get_challenges(2024)
+    html["challenges"] = create_challenges_html(challenges)
 
     print("== home ==")
     print(g.token)
