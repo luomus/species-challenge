7f6e45585a036852ec998fe54504e9e926f944d1
Mikko Heikkinen <hawk@biomi.org>
2024-01-31 14:55:06 +0200

    Show also my trashed participations

--- app/controllers/my.py
+++ app/controllers/my.py
@@ -3,15 +3,28 @@
 from flask import g
 from helpers import common_db
 
-def get_participations(status):
-    params = (status, g.user_data["id"])
-    with common_db.connection() as conn:
-        query = """SELECT p.*, c.*
-            FROM participations p
-            JOIN challenges c ON p.challenge_id = c.challenge_id
-            WHERE c.status = %s
-            AND p.meta_created_by = %s;"""
-        data = common_db.select(conn, query, params)
+def get_participations(status, trashed):
+    if status == "all":
+        params = (g.user_data["id"], trashed)
+        with common_db.connection() as conn:
+            query = """SELECT p.*, c.*
+                FROM participations p
+                JOIN challenges c ON p.challenge_id = c.challenge_id
+                WHERE p.meta_created_by = %s
+                AND p.trashed = %s
+            ;"""
+            data = common_db.select(conn, query, params)
+    else:
+        params = (status, g.user_data["id"], trashed)
+        with common_db.connection() as conn:
+            query = """SELECT p.*, c.*
+                FROM participations p
+                JOIN challenges c ON p.challenge_id = c.challenge_id
+                WHERE c.status = %s
+                AND p.meta_created_by = %s
+                AND p.trashed = %s
+            ;"""
+            data = common_db.select(conn, query, params)
     return data
 
 
@@ -32,10 +45,9 @@ def make_participations_html(participations):
 def main():
     html = dict()
 
-    print(get_participations("open"))
-
-    html["open_participations"] = make_participations_html(get_participations("open"))
-    html["draft_participations"] = make_participations_html(get_participations("draft"))
-    html["closed_participations"] = make_participations_html(get_participations("closed"))
+    html["open_participations"] = make_participations_html(get_participations("open", 0))
+    html["draft_participations"] = make_participations_html(get_participations("draft", 0))
+    html["closed_participations"] = make_participations_html(get_participations("closed", 0))
+    html["trashed_participations"] = make_participations_html(get_participations("all", 1))
 
     return html
