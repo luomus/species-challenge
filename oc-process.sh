9e1df7b28dd66a0e12a737b445cc8a1097d8d431
Mikko Heikkinen <hawk@biomi.org>
2024-03-01 09:25:26 +0200

    Add rahti files to gitignore

---
+++ oc-process.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+i="all"
+
+while getopts ":f:e:i::" flag; do
+case $flag in
+f) f=${OPTARG} ;;
+e) e=${OPTARG} ;;
+i) i=${OPTARG} ;;
+esac
+done
+
+set -a
+
+source ./$e
+
+set +a
+
+BRANCH=$(git symbolic-ref --short -q HEAD)
+
+if [ "$BRANCH" != "main" ]; then
+
+HOST=$HOST_DEV
+
+fi
+
+if [ $i = "volume" ]; then
+
+ITEM=".items[0]"
+
+elif [ $i = "image" ]; then
+
+ITEM=".items[1]"
+
+elif [ $i = "build" ]; then
+
+ITEM=".items[2]"
+
+elif [ $i = "deploy-app" ]; then
+
+ITEM=".items[3]"
+
+elif [ $i = "deploy-db" ]; then
+
+ITEM=".items[4]"
+
+elif [ $i = "service-app" ]; then
+
+ITEM=".items[5]"
+
+elif [ $i = "service-db" ]; then
+
+ITEM=".items[6]"
+
+elif [ $i = "route" ]; then
+
+ITEM=".items[7]"
+
+else
+
+  ITEM=""
+
+fi
+
+oc process -f $f \
+-p BRANCH=$BRANCH \
+-p HOST=$HOST \
+| jq $ITEM
+
